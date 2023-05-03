a38b1aaabdf4886e688a61e9940aca78d0ff27f9
Matviy <kmilyj72@gmail.com>
2023-05-03 12:48:37 +0300

    range input done

--- src/components/dashboard-content/calculator/Calculator.jsx
+++ src/components/dashboard-content/calculator/Calculator.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import './Calculator.scss'
+import React, { useState } from "react";
+import "./Calculator.scss";
+import CalculatorLines from "../../../assets/img/dashboard-icons/calculator icons/calculator-lines.svg";
 
 const Calculator = () => {
-  return (
-    <div className='dashboard-calculator'>
-      <span className='dashboard-item-title'>Calculator</span>
-    </div>
-  )
-}
+  const [currentAmount, setCurrentAmount] = useState(0);
+  const [maxAmount, setMaxAmount] = useState(100000);
 
-export default Calculator
+  const handleSliderChange = (e) => {
+    setCurrentAmount(e.target.value);
+  };
+
+  return (
+    <div className="dashboard-calculator">
+      <span className="dashboard-item-title">Calculator</span>
+      <div className="calculator-percentage">
+        <div className="zero-percent">
+          <span>0%</span>
+        </div>
+        <div className="calculator-current-amount">€{currentAmount}</div>
+        <div className="hundred-percent">
+          <span>€{maxAmount}</span>
+        </div>
+      </div>
+      <div className="calculator-input-range">
+        <input
+          type="range"
+          min="0"
+          max={maxAmount}
+          value={currentAmount}
+          onChange={handleSliderChange}
+          style={{ "--slider-value": currentAmount, "--slider-max": maxAmount }}
+        />
+      </div>
+      <div className="calculator-lines">
+        <img src={CalculatorLines} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
