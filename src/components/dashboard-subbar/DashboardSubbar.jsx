076f321faf8fae775e634be6d7f305a51637891a
Matviy <kmilyj72@gmail.com>
2023-05-07 14:06:49 +0300

    navbar size bug while zooming fixed

--- src/components/dashboard-subbar/DashboardSubbar.jsx
+++ src/components/dashboard-subbar/DashboardSubbar.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import './DashboardSubbar.scss'
-import DashboardSubbarHeader from './dashboard-subbar-header/DashboardSubbarHeader'
-import DashboardSubbarPath from './dashboard-subbar-path/DashboardSubbarPath'
-import DashboardSubbarItems from './dashboard-subbar-items/DashboardSubbarItems'
+import React from "react";
+import "./DashboardSubbar.scss";
+import DashboardSubbarHeader from "./dashboard-subbar-header/DashboardSubbarHeader";
+import DashboardSubbarPath from "./dashboard-subbar-path/DashboardSubbarPath";
+import DashboardSubbarItems from "./dashboard-subbar-items/DashboardSubbarItems";
 
 const DashboardSubbar = () => {
   return (
-    <div className='subbar'>
+    <div className="subbar">
       <DashboardSubbarHeader />
       <DashboardSubbarPath />
       <DashboardSubbarItems />
     </div>
-  )
-}
+  );
+};
 
-export default DashboardSubbar
+export default DashboardSubbar;
