39919ed68155d81e275e52e289e81585c1c9065c
Matviy <kmilyj72@gmail.com>
2023-05-07 13:56:43 +0300

    navbar suppogit add .

--- src/components/navbar/navbarHomepage/NavbarHomepage.jsx
+++ src/components/navbar/navbarHomepage/NavbarHomepage.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import "./NavbarHomepage.scss";
-import DlendoIcon from "../../../assets/img/homepage-icons/dlendo-homepage-icon.svg";
-import QuestionIcon from "../../../assets/img/homepage-icons/question-icon.svg";
 
 const NavbarHomepage = () => {
   return (
     <div className="navbar-homepage">
       <p>HOMEPAGE</p>
       <ul>
-        <li>
-          <img src={DlendoIcon} alt="" /> <span>Dlendo</span>
+        <li className="navbar-homepage-dlendo">
+          <img src="" alt="" /> <span>Dlendo</span>
         </li>
-        <li>
-          <img src={QuestionIcon} className="faq-icon" alt="" /> <span className="faq">FAQ</span>
+        <li className="navbar-homepage-faq">
+          <img src="" className="faq-icon" alt="" />{" "}
+          <span className="faq">FAQ</span>
         </li>
       </ul>
     </div>
--- src/components/navbar/navbarSupport/NavbarSupport.jsx
+++ src/components/navbar/navbarSupport/NavbarSupport.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./NavbarSupport.scss";
-import MailIcon from "../../../assets/img/support-icons/mail-icon.svg";
 import FacebookIcon from "../../../assets/img/support-icons/facebook-icon.svg";
 
 const NavbarSupport = () => {
@@ -8,11 +7,11 @@ const NavbarSupport = () => {
     <div className="navbar-support">
       <p>SUPPORT</p>
       <ul>
-        <li>
-          <img src={MailIcon} alt="" /> <span>Support@dlendo.com</span>
+        <li className="navbar-support-mail">
+          <img src="" alt="" /> <span>Support@dlendo.com</span>
         </li>
         <li>
-          <img src={FacebookIcon} alt="" className="fb-icon"/>
+          <img src={FacebookIcon} alt="" className="fb-icon" />
           <span className="fb-text">Facebook</span>
         </li>
       </ul>
